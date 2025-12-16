4e58c70e6cc2b3c054584a591189cdb524c11d7e
Le Kien <kienle371999@gmail.com>
2025-12-16 09:55:37 +0700

    Comment out unused functions in the refined

--- smartinv.py
+++ smartinv.py
@@ -302,7 +302,7 @@ def run_refined_exp():
 			#run_veriSmart(file_results_dir, contract_file, filename)
 			#run_smartest(file_results_dir, contract_file, filename)
 			#run_manticore(file_results_dir, contract_file, filename)			
-	sets = ["set1", "set2", "set3"]
+	sets = ["set1"]
 	for i in sets:
 		test_folder_path = f"/opt/home/s4160469/SmartInv/tests/refined_analysis/natural_bugs/{i}"
 		for root, dirs, files in os.walk(audited_bug_folder_path):
@@ -310,13 +310,13 @@ def run_refined_exp():
 				if ".sol" not in filename:
 					break
 				contract_file = os.path.join(root, filename)
-				run_verisol(file_results_dir, contract_file, filename)
-				run_slither(file_results_dir, contract_file, filename)
-				#infer_bugs(test_folder_path, file_results_dir, smartinv_exp_name, smartinv_prompt, line_limit) 					
+				# run_verisol(file_results_dir, contract_file, filename)
+				# run_slither(file_results_dir, contract_file, filename)
+				infer_bugs(test_folder_path, file_results_dir, smartinv_exp_name, smartinv_prompt, line_limit) 					
 				#comment out the run_mythril, run_manticore, run_veriSmart and run_smartest for linux machine, unless you get 
 				#verismart working on linux; only use the following two commands on MacOS 
 				#the following command has to be in the test_dir, not outside it
-				run_mythril(file_results_dir, contract_file, filename)
+				# run_mythril(file_results_dir, contract_file, filename)
 				#run_veriSmart(file_results_dir, contract_file, filename)
 				#run_smartest(file_results_dir, contract_file, filename)
 				#run_manticore(file_results_dir, contract_file, filename)

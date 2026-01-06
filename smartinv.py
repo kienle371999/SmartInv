148f2555b0fa90e9ac0cf3188cbfa22b2555f133
Le Kien <kienle371999@gmail.com>
2026-01-06 10:01:37 +0700

    fix: test the OneRing folder

--- smartinv.py
+++ smartinv.py
@@ -302,7 +302,7 @@ def run_refined_exp():
 	# 		#run_veriSmart(file_results_dir, contract_file, filename)
 	# 		#run_smartest(file_results_dir, contract_file, filename)
 	# 		#run_manticore(file_results_dir, contract_file, filename)			
-	sets = ["set1"]
+	sets = ["set2/OneRing"]
 	for i in sets:
 		test_folder_path = f"/opt/home/s4160469/SmartInv/tests/refined_analysis/natural_bugs/{i}"
 		for root, dirs, files in os.walk(audited_bug_folder_path):
